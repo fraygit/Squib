4b379b48afc5dc2cb6cbcd02b80439fccce82c71
francis.yanga@gmail.com <francis.yanga@gmail.com>
2018-03-14 10:28:33 +1300

    user repository

---
+++ Squib.Data/Model/User.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using Squib.Data.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Squib.Data.Model
+{
+    public class User : MongoEntity
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ProfilePhoto { get; set; }
+        public string ProfilePhotoFilename { get; set; }
+        public string Status { get; set; } 
+        public string VerificationCode { get; set; }
+        public string CurrentOrganisation { get; set; }
+        public List<ObjectId> Organisations { get; set; }
+    }
+}
---
+++ Squib.Data/Repository/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Squib.Data.Interface;
+using Squib.Data.Model;
+using Squib.Data.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Squib.Data.Repository
+{
+    public class UserRepository : EntityService<User>, IUserRepository
+    {
+    }
+}
