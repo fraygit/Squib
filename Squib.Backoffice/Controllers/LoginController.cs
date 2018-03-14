b0d5ae0f6a41a06f9ced473de0e6e5e2a2680447
francis.yanga@gmail.com <francis.yanga@gmail.com>
2018-03-14 22:57:31 +1300

    login

--- Squib.Backoffice/Controllers/LoginController.cs
+++ Squib.Backoffice/Controllers/LoginController.cs
@@ -1,19 +1,52 @@
-﻿using System;
+﻿using Squib.Backoffice.Models;
+using Squib.Data.Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace Squib.Backoffice.Controllers
 {
     public class LoginController : Controller
     {
-        //
-        // GET: /Login/
+        private IUserRepository _userRepository;
 
-        public ActionResult Index()
+        public LoginController(IUserRepository _userRepository)
         {
-            return View();
+            this._userRepository = _userRepository;
+        }
+
+        public ActionResult Index(ReqLogin request)
+        {
+            return View(request);
+        }
+
+        public async Task<ActionResult> Validate(ReqLogin loginModel)
+        {
+            ViewBag.ErrorMessage = "";
+            var existingUser = await _userRepository.GetUser(loginModel.Username);
+            if (existingUser != null)
+            {
+                if (existingUser.Status == "Registered")
+                {
+                    ViewBag.ErrorMessage = "Account not yet activated.";
+                    return RedirectToAction("Index", new LoginModel { Message = "Email not verified." });
+                }
+            }
+
+            var isUserValid = Membership.ValidateUser(loginModel.Username, loginModel.Password);
+            if (isUserValid)
+            {
+                FormsAuthentication.SetAuthCookie(loginModel.Username, false);
+                return RedirectToAction("Index", "Dashboard");
+            }
+            else
+            {
+                return RedirectToAction("Index", new LoginModel { Message = "Invalid username or password." });
+            }
         }
 
     }
---
+++ Squib.Backoffice/Models/ReqLogin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Squib.Backoffice.Models
+{
+    public class ReqLogin
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool IsInvalid { get; set; }
+        public string Message { get; set; }
+    }
+}
