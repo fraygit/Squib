aed622731ed4befa3eca593c08f03fab58cf559b
francis.yanga@gmail.com <francis.yanga@gmail.com>
2018-03-15 15:15:17 +1300

    promot data layer

---
+++ Squib.Data/Model/Promo.cs
@@ -0,0 +1,18 @@
+﻿using Squib.Data.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Squib.Data.Model
+{
+    public class Promo : MongoEntity
+    {
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string MainPhoto { get; set; }
+        public string Address { get; set; }
+    }
+}
