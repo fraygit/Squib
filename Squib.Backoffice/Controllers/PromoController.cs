9a12a1e8bc70590f27ebb9e5a9d1eddcd74026ac
francis.yanga@gmail.com <francis.yanga@gmail.com>
2018-03-20 13:29:37 +1300

    promo list data table

--- Squib.Backoffice/Controllers/PromoController.cs
+++ Squib.Backoffice/Controllers/PromoController.cs
@@ -57,6 +57,34 @@ namespace Squib.Backoffice.Controllers
         }
 
         [Authorize]
+        public async Task<JsonResult> PromoList()
+        {
+            var user = await _userRepository.GetUser(User.Identity.Name);
+            var promos = new List<ResPromoList>();
+            foreach (var org in user.Organisations)
+            {
+                var organisation = await _organisationRepository.Get(org.ToString());
+                var orgPromos = await _promoRepository.GetByOrg(org);
+                foreach (var promo in orgPromos)
+                {
+                    promos.Add(new ResPromoList
+                    {
+                        Id = promo.Id,
+                        Category = promo.Category,
+                        Title = promo.Title,
+                        From = promo.From,
+                        To = promo.To,
+                        OrganisationName = organisation.Name,
+                        MaxNumberOfVoucher = promo.MaxNumberOfVoucher
+                    });
+                }
+            }
+            return Json(promos, JsonRequestBehavior.AllowGet);
+        }
+
+
+
+        [Authorize]
         public async Task<ActionResult> Create(string id)
         {
             var model = new CreatePromoViewModel();
