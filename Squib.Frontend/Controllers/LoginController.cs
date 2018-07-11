a6c010aec9678f3ae0bc37375f2ac08a07081966
fray <francis.yanga@foundationfootprint.com>
2018-07-12 09:23:49 +1200

    register

--- Squib.Frontend/Controllers/LoginController.cs
+++ Squib.Frontend/Controllers/LoginController.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using Squib.Data.Interface;
+using Squib.Data.Model;
+using Squib.Data.Security;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
@@ -9,6 +14,13 @@ namespace Squib.Frontend.Controllers
 {
     public class LoginController : Controller
     {
+        private IUserRepository _userRepository;
+
+        public LoginController(IUserRepository _userRepository)
+        {
+            this._userRepository = _userRepository;
+        }
+
         // GET: Login
         public ActionResult Index()
         {
@@ -19,6 +31,45 @@ namespace Squib.Frontend.Controllers
         {
             FormsAuthentication.SignOut();
             return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Register(string email, string password, string passwordRepeat, string firstName, string lastName)
+        {
+            if (password != passwordRepeat)
+            {
+                ViewBag.Message = "Password does not match";
+                return View();
+            }
+            if (!email.Contains("@") || !email.Contains("."))
+            {
+                ViewBag.Message = "Please enter a valid email address";
+                return View();
+            }
+            var findExistingEmail = await _userRepository.GetUser(email);
+            if (findExistingEmail == null)
+            {
+
+                var user = new User
+                {
+                    Email = email,
+                    FirstName = firstName,
+                    LastName = lastName,
+                    VerificationCode = ObjectId.GenerateNewId().ToString(),
+                    Password = SquibCrypto.HashSha256(password)
+                };
+                await _userRepository.CreateSync(user);
+
+                FormsAuthentication.SetAuthCookie(email, false);
+                return RedirectToAction("Index", "Home");
+            }
+            ViewBag.Message = "Email with the same user already exists.";
+            return View();
         }
 
         [HttpPost]
